7625f840011ab552dea4815673821b7473bc76a3
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-05 00:15:56 +0530

    next() function aand mutilple routes

--- src/app.js
+++ src/app.js
@@ -2,16 +2,20 @@ const express=require("express")
 
 const app=express();
 
-app.use("/hello",(req,res)=>{
-  res.send("ahhahaha")
-})
-app.get("/hello",(req,res)=>{
-  res.send("hello from the sever")
-})
 
-app.post(
-  "/hello",(req,res)=>{
-  res.send("data saved")
+app.use("/user",(req,res,next)=>{
+  console.log("1st");
+  
+  next()
+},(req,res,next)=>{
+  console.log("2nd");
+  
+  next()
+})
+app.use("/user",(req,res,next)=>{
+  console.log("3rd");
+
+  
 })
 app.listen(3000,()=>{
   console.log(" server listening to the port 3000...");
