23ff302c2d30359f50816c194be0abdf516ddb4a
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-04 23:25:50 +0530

    exploring the routing and request handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express=require("express")
 
 const app=express();
 
+app.use("/hello",(req,res)=>{
+  res.send("ahhahaha")
+})
+app.get("/hello",(req,res)=>{
+  res.send("hello from the sever")
+})
 
-
-app.use("/test",(req,res)=>{
-  res.send("hello world")
+app.post(
+  "/hello",(req,res)=>{
+  res.send("data saved")
 })
 app.listen(3000,()=>{
   console.log(" server listening to the port 3000...");
